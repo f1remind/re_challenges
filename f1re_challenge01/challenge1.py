3e820eaeae9ba3665c597adf27b7506261b4424a
Jan-Niklas Nowak <drowned@web.de>
2016-10-07 00:23:06 +0200

    Added first challenge
    The first challenge has been added!
    Slightly obfuscated, should be pretty doable.
    The hardest thing is knowing the python syntax.

--- f1re_challenge01/challenge1.py
+++ f1re_challenge01/challenge1.py
@@ -1,1 +1,20 @@
-#initialisation
+def check(z):
+    a = 0
+    for b in range(len(z)):
+        a += ord(z[b])*b
+    return a==13688
+
+def main():
+    password = input("Please enter the passphrase: ")
+    login = check(password)
+    if login:
+        print("Password accepted, welcome!")
+    else:
+        print("Wrong password!")
+    return
+
+
+'''Standard code to start the main function if the module
+has been started directly and not just imported'''
+if __name__ == '__main__':
+    main()
